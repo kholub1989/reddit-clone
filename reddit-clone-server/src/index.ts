9dfe729cd0a1fd8d2512d8e3616205b72c69647e
Krisztian Holub <kholub1989@gmail.com>
2020-10-28 12:14:47 -0400

    Register Resolver

--- reddit-clone-server/src/index.ts
+++ reddit-clone-server/src/index.ts
@@ -1,13 +1,14 @@
 import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+
 import microConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolvers } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
+import { UserResolvers } from "./resolvers/user";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -17,7 +18,7 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolvers, PostResolver],
+      resolvers: [HelloResolvers, PostResolver, UserResolvers],
       validate: false,
     }),
     context: () => ({ em: orm.em }),
