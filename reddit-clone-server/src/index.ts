04ca6f307ef3ff26c85f76ae852dad6d9b221a5a
Krisztian Holub <kholub1989@gmail.com>
2020-10-27 21:48:38 -0400

    Add Apollo Server and Express Setup

--- reddit-clone-server/src/index.ts
+++ reddit-clone-server/src/index.ts
@@ -2,15 +2,29 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolvers } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, { title: "my first post" });
-  // await orm.em.persistAndFlush(post);
+  const PORT = 4000;
+  const app = express();
 
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolvers],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(PORT, () => {
+    console.log(`server is running on localhost:${PORT}`);
+  });
 };
 main().catch((err) => {
   console.error(err);
