1740ff2dafc1b239cd5baad71a340a863fb29b41
Krisztian Holub <kholub1989@gmail.com>
2020-11-02 16:44:54 -0500

    add forgot password functionalety

--- reddit-clone-web/src/pages/login.tsx
+++ reddit-clone-web/src/pages/login.tsx
@@ -15,10 +15,10 @@ const Login: React.FC<{}> = ({}) => {
   return (
     <Wrapper variant="small">
       <Formik
-        initialValues={{ username: "", password: "" }}
+        initialValues={{ usernameOrEmail: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
           console.log(values);
-          const response = await login({ options: values });
+          const response = await login(values);
           if (response.data?.login.errors) {
             setErrors(toErrorMap(response.data.login.errors));
           } else if (response.data?.login.user) {
@@ -30,9 +30,9 @@ const Login: React.FC<{}> = ({}) => {
         {({ isSubmitting }) => (
           <Form>
             <InputField
-              name="username"
-              placeholder="username"
-              label="username"
+              name="usernameOrEmail"
+              placeholder="username or email"
+              label="username or email"
             />
             <Box mt={4}>
               <InputField
--- reddit-clone-web/src/pages/register.tsx
+++ reddit-clone-web/src/pages/register.tsx
@@ -17,10 +17,10 @@ const Register: React.FC<registerProps> = ({}) => {
   return (
     <Wrapper variant="small">
       <Formik
-        initialValues={{ username: "", password: "" }}
+        initialValues={{ email: "", username: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
           console.log(values);
-          const response = await register(values);
+          const response = await register({ options: values });
           if (response.data?.register.errors) {
             setErrors(toErrorMap(response.data.register.errors));
           } else if (response.data?.register.user) {
@@ -34,13 +34,16 @@ const Register: React.FC<registerProps> = ({}) => {
             <InputField
               name="username"
               placeholder="username"
-              label="username"
+              label="Username"
             />
+            <Box mt={4}>
+              <InputField name="email" placeholder="email" label="Email" />
+            </Box>
             <Box mt={4}>
               <InputField
                 name="password"
                 placeholder="password"
-                label="password"
+                label="Password"
                 type="password"
               />
             </Box>
