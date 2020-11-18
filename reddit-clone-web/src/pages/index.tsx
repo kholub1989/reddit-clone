47dd4503cda40926e589dd09645dfa530f4983e0
Krisztian Holub <kholub1989@gmail.com>
2020-11-18 15:33:09 -0500

    Invalidate Queries

--- reddit-clone-web/src/pages/index.tsx
+++ reddit-clone-web/src/pages/index.tsx
@@ -9,7 +9,7 @@ import NextLink from "next/link";
 // rerun
 const Index = () => {
   const [variables, setVariables] = useState({
-    limit: 10,
+    limit: 15,
     cursor: null as null | string,
   });
   console.log(variables);
@@ -39,6 +39,7 @@ const Index = () => {
           {data!.posts.posts.map((p) => (
             <Box key={p.id} p={5} shadow="md" borderWidth="1px">
               <Heading fontSize="xl">{p.title}</Heading>
+              <Text>posted by {p.creator.username}</Text>
               <Text mt={4}>{p.textSnippet}</Text>
             </Box>
           ))}
