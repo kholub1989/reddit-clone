5f2e9e18b75ea1cee527915149a6bb3020ec64bb
Krisztian Holub <kholub1989@gmail.com>
2020-11-20 12:13:08 -0500

    add Upvote UI

--- reddit-clone-web/src/pages/index.tsx
+++ reddit-clone-web/src/pages/index.tsx
@@ -3,7 +3,18 @@ import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
 import { Layout } from "../components/Layout";
-import { Box, Link, Stack, Heading, Text, Flex, Button } from "@chakra-ui/core";
+import { UpdootSection } from "../components/UpdootSection";
+import {
+  Box,
+  Link,
+  Stack,
+  Heading,
+  Text,
+  Flex,
+  Button,
+  Icon,
+  IconButton,
+} from "@chakra-ui/core";
 import NextLink from "next/link";
 
 // rerun
@@ -37,11 +48,14 @@ const Index = () => {
       ) : (
         <Stack spacing={8}>
           {data!.posts.posts.map((p) => (
-            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
-              <Heading fontSize="xl">{p.title}</Heading>
-              <Text>posted by {p.creator.username}</Text>
-              <Text mt={4}>{p.textSnippet}</Text>
-            </Box>
+            <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
+              <UpdootSection post={p} />
+              <Box>
+                <Heading fontSize="xl">{p.title}</Heading>
+                <Text>posted by {p.creator.username}</Text>
+                <Text mt={4}>{p.textSnippet}</Text>
+              </Box>
+            </Flex>
           ))}
         </Stack>
       )}
