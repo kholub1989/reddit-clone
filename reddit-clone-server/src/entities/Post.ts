9144ca500787c5916499abfebe3f9459e9611157
Krisztian Holub <kholub1989@gmail.com>
2020-11-23 11:07:47 -0500

    Write Fragment

--- reddit-clone-server/src/entities/Post.ts
+++ reddit-clone-server/src/entities/Post.ts
@@ -1,4 +1,4 @@
-import { Field, ObjectType } from "type-graphql";
+import { Field, Int, ObjectType } from "type-graphql";
 import {
   Entity,
   Column,
@@ -31,6 +31,9 @@ export class Post extends BaseEntity {
   @Column({ type: "int", default: 0 })
   points!: number;
 
+  @Field(() => Int, { nullable: true })
+  voteStatus: number | null; // 1 or -1 or null
+
   @Field()
   @Column()
   creatorId: number;
@@ -38,7 +41,7 @@ export class Post extends BaseEntity {
   @Field()
   @ManyToOne(() => User, (user) => user.posts)
   creator: User;
-   
+
   @OneToMany(() => Updoot, (updoot) => updoot.post)
   updoots: Updoot[];
 
--- reddit-clone-web/src/utils/createUrqlClient.ts
+++ reddit-clone-web/src/utils/createUrqlClient.ts
@@ -12,8 +12,10 @@ import {
   MeDocument,
   MeQuery,
   RegisterMutation,
+  VoteMutationVariables,
 } from "../generated/graphql";
 import { betterUpdateQuery } from "./betterUpdateQuery";
+import gql from "graphql-tag";
 import Router from "next/router";
 
 const errorExchange: Exchange = ({ forward }) => (ops$) => {
@@ -132,6 +134,29 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
       updates: {
         Mutation: {
+          vote: (_result, args, cache, info) => {
+            const { postId, value } = args as VoteMutationVariables;
+            const data = cache.readFragment(
+              gql`
+                fragment _ on Post {
+                  id
+                  points
+                }
+              `,
+              { id: postId } as any
+            );
+            if (data) {
+              const newPoints = (data.points as number) + value;
+              cache.writeFragment(
+                gql`
+                  fragment __ on Post {
+                    points
+                  }
+                `,
+                { id: postId, points: newPoints } as any
+              );
+            }
+          },
           createPost: (_result, args, cache, info) => {
             const allFields = cache.inspectFields("Query");
             const fieldInfos = allFields.filter(
